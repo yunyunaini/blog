bbbbd6cc8035d4aba3d62e5b085e56224f41c77c
wusimin7 <972774037@qq.com>
2020-12-31 23:05:53 +0800

    fix: 日志输出文件

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 sudo /usr/sbin/sshd
-sudo mkdir -p /export/logs
+sudo mkdir -p /export/App/logs
 sudo mkdir -p /dev/shm/nginx_temp/client_body
 cp ../nginx.conf /export/servers/nginx/conf/domains
 sudo /export/servers/nginx/sbin/nginx -c /export/servers/nginx/conf/nginx.conf &> /export/Logs/node_install.log
